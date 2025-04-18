fc5f1b17581d1a6d53ffb32d6a77e048774cbcf3
Akash Dhasade <akash.dhasade@epfl.ch>
2025-04-18 09:26:15 +0200

    scripts

---
+++ scripts/run_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+root_dir=/home/dhasade/ragroute
+save_dir=mtl
+log_dir=$root_dir/results/$save_dir
+mkdir -p $log_dir 
+nfs_dir=/scratch/home/dhasade/ragroute
+
+echo "==> Updating code from nfs"
+
+cp -r ${nfs_dir}/ragroute $root_dir
+cp -r ${nfs_dir}/*.py $root_dir
+
+env_python=/home/dhasade/.conda/envs/rag/bin/python
+
+echo "==> Running server"
+
+$env_python $root_dir/main.py
