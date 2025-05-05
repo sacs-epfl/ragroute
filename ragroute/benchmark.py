256891b98fa69b3138d86222f3bb5ea5942db564
Martijn de Vos <martijn.devos@epfl.ch>
2025-05-05 11:36:11 +0200

    Added MIRAGE answer verification

--- ragroute/benchmark.py
+++ ragroute/benchmark.py
@@ -1,5 +1,7 @@
 import json
 import os
+import re
+from typing import Dict
 
 from ragroute.config import USR_DIR
 
@@ -10,3 +12,54 @@ class Benchmark:
         benchmark_file = os.path.join(USR_DIR, benchmark_name, "benchmark.json")
         with open(benchmark_file, 'r') as f:
             self.benchmark_data = json.load(f)
+
+    def check_mirage_answer(self, data_question: Dict, llm_output: str) -> bool:
+        def locate_answer(sentence: str):
+            ans = re.findall(r"^\s*(A|B|C|D)$", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+            
+            ans = re.findall(r"^\s*(A|B|C|D) or", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+            
+            ans = re.findall(r"^\s*(A|B|C|D) and", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+                
+            ans = re.findall(r"^\s*(A|B|C|D)/", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+            
+            ans = re.findall(r"^\s*(A|B|C|D),", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+            
+            ans = re.findall(r"[Oo]ption (A|B|C|D)", sentence)
+            if len(ans) > 0:
+                return ans[0]
+
+            ans = re.findall(r":\s*(A|B|C|D)", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+
+            ans = re.findall(r"^\s*(A|B|C|D)\.", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+
+            ans = re.findall(r"^\s*(A|B|C|D)\"", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+            
+            ans = re.findall(r"^\s*(A|B|C|D):", sentence)
+            if len(ans) > 0:
+                return ans[0].upper()
+            return "A"
+
+        answer_list = ["A", "B", "C", "D"]
+
+        ans = locate_answer(llm_output.split('"answer_choice": "')[-1].strip())
+
+        if ans in answer_list and data_question["answer"] in answer_list:
+            return ans == data_question["answer"]
+        return False
--- run_benchmark.py
+++ run_benchmark.py
@@ -14,16 +14,16 @@ async def main():
     parser.add_argument("--benchmark", type=str, default="MIRAGE", choices=["MIRAGE"], help="Benchmark name")
     args = parser.parse_args()
 
-    questions: int = 0
-    correct: int = 0
+    num_questions: int = 0
+    num_correct: int = 0
     
     # Load the benchmark
     benchmark = Benchmark(args.benchmark)
     async with aiohttp.ClientSession() as session:
         for dataset_name, questions in benchmark.benchmark_data.items():
-            for question_id, data in tqdm(questions.items()):
-                question = data['question']
-                options = data['options']
+            for question_id, question_data in tqdm(questions.items()):
+                question = question_data['question']
+                options = question_data['options']
 
                 # Fire the HTTP request using asyncio
 
@@ -36,6 +36,14 @@ async def main():
                     if response.status == 200:
                         json_response = await response.json()
                         print(json_response)
+
+                        # Check if the answer is correct
+                        is_correct: bool = benchmark.check_mirage_answer(question_data, json_response["answer"])
+                        if is_correct:
+                            num_correct += 1
+                        num_questions += 1
+                            
+                        print("--> Score: %d/%d" % (num_correct, num_questions))
                     else:
                         print(f"Failed to fetch data for {question_id}: {response.status} - {await response.text()}")
 
